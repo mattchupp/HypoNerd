0f4e2959c0a40e279fbfeef308c50c65c40493f5
Matthew Chupp <mattlchupp@gmail.com>
2015-04-14 09:33:38 -0500

    keyframe animations

--- MCHypnosisViewController.m
+++ MCHypnosisViewController.m
@@ -96,6 +96,18 @@
                             }
                          completion:NULL];
         
+        [UIView animateKeyframesWithDuration:1.0 delay:0.0 options:0 animations:^{
+            [UIView addKeyframeWithRelativeStartTime:0 relativeDuration:0.8 animations:^{
+                messageLabel.center = self.view.center;
+            }];
+            
+            [UIView addKeyframeWithRelativeStartTime:0.8 relativeDuration:0.2 animations:^{
+                int x = arc4random() % width;
+                int y = arc4random() % height;
+                messageLabel.center = CGPointMake(x, y);
+            }];
+        }completion:NULL];
+        
         // parallax to messageLabel
         UIInterpolatingMotionEffect *motionEffect;
         motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
